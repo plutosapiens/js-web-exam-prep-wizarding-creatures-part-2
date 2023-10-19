b838296d95a66fb726ede9bf797e74eb7320e5f4
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-10-19 17:04:05 +0300

    WIZ-2 Add edit and delete functionality

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -18,7 +18,6 @@ router.get('/profile', (req, res) => {
 
 router.post('/create', async (req, res) => {
     const { name, species, skinColor, eyeColor, image, description, } = req.body
-
     const payload = {  
         name, 
         species, 
@@ -27,9 +26,11 @@ router.post('/create', async (req, res) => {
         image, 
         description, 
         owner: req.user, 
-    }
+    };
 
     await creatureService.create(payload);
+    console.log(payload)
+    console.log(req.body)
     
     res.redirect ('/posts/all')
 });
@@ -45,5 +46,39 @@ router.get('/:creatureId/details', async (req, res) => {
 
     res.render('post/details', { creature, isOwner });
 });
+
+router.get('/:creatureId/edit', async (req, res) => {
+    const { creatureId } = req.params;
+    const creature = await creatureService.singleCreature(creatureId).lean();
+
+    res.render('post/edit', { creature });
+});
+
+router.post('/:creatureId/edit', async (req, res) => {
+    const { creatureId } = req.params;
+    const { name, species, skinColor, eyeColor, image, description, } = req.body;
+    
+    const payload = {  
+        name, 
+        species, 
+        skinColor, 
+        eyeColor, 
+        image, 
+        description, 
+        owner: req.user, 
+    };
+
+    await creatureService.update(creatureId, payload);
+    res.redirect(`/posts/${creatureId}/details`);
+});
+
+router.get('/:creatureId/delete', async (req, res) => {
+    const { creatureId } = req.params;
+    await creatureService.delete(creatureId)
+
+    res.redirect('/posts/all');
+});
+
+
 
 module.exports = router;
--- src/services/creatureService.js
+++ src/services/creatureService.js
@@ -6,3 +6,8 @@ exports.create = (createData) => Creature.create(createData);
 exports.getAll = () => Creature.find();
 
 exports.singleCreature = (creatureId) => Creature.findById(creatureId);
+
+exports.update = (creatureId, creatureData) => 
+Creature.findByIdAndUpdate(creatureId, creatureData);
+
+exports.delete = (creatureId) => Creature.findByIdAndDelete(creatureId);
